d57f8b5b01c685916062d4a9a3a759b92a8545bb
juandavid1503 <166643564+juandavid1503@users.noreply.github.com>
2024-11-11 23:28:44 -0500

    js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const products = [
   { id: 1, name: 'Hamburguesa isleña', price: 15000 },
   { id: 2, name: 'Hamburguesa costeña', price: 15000 },
-  { id: 3, name: 'Hamburguesa gaucha', price: 12000 },
+  { id: 3, name: 'Hamburguesa gaucha', price: 15000 },
   { id: 4, name: 'Hamburguesa doble carne', price: 20000 },
-  { id: 5, name: 'Choripan', price: 10000 },
-  { id: 6, name: 'Coca cola', price: 4000 },
-  { id: 7, name: 'Sprite', price: 4000 },
-  { id: 8, name: 'Corona', price: 7000 },
-  { id: 9, name: 'Francesa', price: 4000 },
-  { id: 10, name: 'Casco', price: 4000 },
+  { id: 5, name: 'Hamburguesa especial', price: 12000 },
+  { id: 6, name: 'Hamburguesa de temporada', price: 18000 },
+  { id: 7, name: 'Hamburguesa jalisco', price: 15000 },
+  { id: 8, name: 'Hamburguesa rosario', price: 17000 },
+  { id: 9, name: 'Hamburguesa sencilla', price: 10000 },
+  { id: 10, name: 'Hamburguesa pollo', price: 18000 },
 ];
 
 let cart = [];
@@ -27,7 +27,6 @@ products.forEach(product => {
   productList.appendChild(productEl);
 });
 
-// Agregar producto al carrito
 function addToCart(productId) {
   const product = products.find(p => p.id === productId);
   const cartItem = cart.find(item => item.id === productId);
@@ -40,7 +39,6 @@ function addToCart(productId) {
   renderCart();
 }
 
-// Renderizar el carrito
 function renderCart() {
   const cartItemsContainer = document.getElementById('cart-items');
   cartItemsContainer.innerHTML = '';
@@ -62,7 +60,6 @@ function renderCart() {
   document.getElementById('total-price').innerText = `Total: $${total}`;
 }
 
-// Actualizar la cantidad de un producto en el carrito
 function updateQuantity(productId, change) {
   const cartItem = cart.find(item => item.id === productId);
   if (cartItem) {
@@ -87,7 +84,7 @@ function checkout() {
     alert('El carrito está vacío.');
     return;
   }
-  alert('¡Pedido realizado con éxito!');
+  alert('¡Pago éxitoso!');
   cart = [];
   renderCart();
 }
